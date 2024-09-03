78147c0c45140ebb502fad38c5bbe6ea1356b603
ankurbansal15 <ankurbansal153@gmail.com>
2024-09-03 22:29:10 +0530

    First commit done

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const {urlencoded} = require("express");
 const app = express();
 const PORT = 8080;
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override")
 
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"))
 
@@ -46,7 +48,7 @@ app.get("/posts/:id",(req,res)=>{
 app.get("/posts/:id/edit",(req,res)=>{
     let {id} = req.params;
     let post = posts.find((p)=>id===p.id)
-
+    res.render("edit.ejs",{post})
 })
 app.patch("/posts/:id",(req,res)=>{
     let {id} = req.params;
@@ -54,7 +56,14 @@ app.patch("/posts/:id",(req,res)=>{
     let post = posts.find((p)=>id===p.id);
     post.content = newContent;
     console.log(post)
-    res.send("patch request working")
+    res.redirect("/posts")
+})
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=>id !== p.id);
+    res.redirect("/posts")
+
+
 })
 app.post("/posts",(req,res)=>{
     let {username,content} = req.body;
